37603f29884a01b0a3938169b135707634e597db
Moises Alcahe <moitoas@gmail.com>
2025-11-19 09:16:54 +0200

    uploadfunc

---
+++ week2/day3/func.py
@@ -0,0 +1,10 @@
+#func.py
+#Create a func.py file with a function that sums two numbers and prints the result. 
+# Then, import and call the function from exercise_one.py.
+
+
+#Define a function inside that file that takes two numbers as arguments, sums them,
+#  and prints the result.
+def sum(x , y):
+    result = x + y
+    print(result)
