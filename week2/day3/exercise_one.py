ed5171a94fea100794b119313f0e1787ddfcfae1
Moises Alcahe <moitoas@gmail.com>
2025-11-19 09:18:05 +0200

    upload exerciseone

---
+++ week2/day3/exercise_one.py
@@ -0,0 +1,9 @@
+#exercise_one.py.
+#Import the function from func.py using one of the import syntaxes provided in the instructions.
+# Call the imported function with two numbers.
+from func import sum
+
+sum(2,4)
+sum(4,4)
+sum(5,5)
+
