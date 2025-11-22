43d9bf819579a140dc2ddd4fd7e33ec0058c26e1
Moises Alcahe <moitoas@gmail.com>
2025-11-22 13:00:24 +0200

    upload

---
+++ week2/day5/betspractice.py
@@ -0,0 +1,8 @@
+#Best Practices in organizing the class
+
+#Class
+#__init__
+#decorators (@classmethod, @property, @stationmethod, etc)
+#regular methods
+#dunder methods (starts with __str__(), __repr__(), and then others)
+
